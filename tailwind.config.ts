1a9134acf48bb98f14418b911ad65a611732da21
thanhnd2512@gmail.com <thanhnd2512@gmail.com>
2024-01-06 11:22:17 +0700

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,31 +14,16 @@ const config: Config = {
       fontFamily: {
         //system font
         LexendDeca: ["Lexend Deca", "sans-serif"],
-        UTMAmerican: ["UTMAmerican", "sans-serif"],
-        UTMFleur: ["UTMFleur", "sans-serif"],
+
+        //custom font
       },
 
       colors: {
-        //system colors
-        redPrimmary: "#ed1b2e",
-        Blue3D: "#1D1D3D",
-        colortopdownBlue: "#2c95ff",
-        colortopdownGray: "#414045",
-        blueAdmin: "#74affc",
-        purpleAdmin: "#bb86fc37",
-        purpleHover: "#BB86FC",
-        BlueFF: "#1A49FF",
-        //Custom colors
-        main: "#1e7147",
-        mainyellow: "#ffd205",
-        mainorange: "#ffb301",
-        mainblue: "#0072ba",
-        mainNormalBlue: "#004769",
-      },
+        //system color
+        adminOrange: "#ffb301",
 
-      // custom
-      backgroundImage: {},
-      backgroundColor: {},
+        //custom color
+      },
     },
     keyframes: {
       shimmer: {
